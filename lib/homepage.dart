86623bbfdaed1772aea97d9cc4cb819123bd41a2
Neeti wason <neetiwasona734@gmail.com>
2023-06-29 13:24:54 +0530

    Day2 Completed

---
+++ lib/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String name = 'Neeti wason';
+  final int days = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hii $name here for $days"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog_1/homepage.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: Material(
-     child: Center( child: Container(
-        child: Text("Hii Neeti wason here"),
-           ),
-         ),
-      )
+      home: HomePage(),
     );
   }
 }
